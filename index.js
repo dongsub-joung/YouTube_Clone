6167f9b339356d1785d044b92ddb00cb876d75e8
dongsub-joung <59364300+dongsub-joung@users.noreply.github.com>
2020-06-20 12:00:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ function handleListening()
   console.log(`Listeing on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) //request obj, response obj
+{
+  //console.log(res);
+  res.send("Hi form Home!!");
+} // 변수가 없으면 서버는 무한 Lording
+
+app.get("/", handleHome);
+
 app.listen(PORT,handleListening);
