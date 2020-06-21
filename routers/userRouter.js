ad5653ca696523a0faccc3bec07875c4469d132c
dongsub-joung <59364300+dongsub-joung@users.noreply.github.com>
2020-06-21 13:57:04 +0900

    1

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router();
+userRouter.get(routes.users, (req,res) => res.send("Users"));
+userRouter.get(routes.userDetail, (req,res) => res.send("UsersDetail"));
+userRouter.get(routes.editProfile, (req,res) => res.send("editeProfile"));
+userRouter.get(routes.changePassword, (req,res) => 
+res.send("changePassword"));
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import routes from "../routes";
 
 const vidoRouter = express.Router();
+
+vidoRouter.get(routes.videos, (req,res) => res.send("videos"));
+vidoRouter.get(routes.upload, (req,res) => res.send("videosUpload"));
+vidoRouter.get(routes.videoDetail, (req,res) => res.send("videosDetail"));
+vidoRouter.get(routes.editVideo, (req,res) => res.send("editevideos"));
+vidoRouter.get(routes.deleteVideo, (req,res) => res.send("DeleteVideos"));
 
 export default vidoRouter;
